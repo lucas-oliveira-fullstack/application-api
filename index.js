32ddbd983b6d20791a42c680d2f2afed47ab9804
Lucas Oliveira <loliveira@iMac-de-Lucas.bbrouter>
2023-07-02 01:37:29 -0300

    Optimizing index.js

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 const express = require('express');
 const db = require('./models/db');
-const userRoutes = require('./routes/user_routes');
-const marketRoutes = require('./routes/user_routes');
+const fs = require('fs');
+const path = require('path');
 
-const index = express();
+const app = express();
+const port = 8000;
 
-index.use(express.json());
+app.use(express.json());
 
-index.use('/routes', userRoutes);
-index.use('/routes', marketRoutes);
+// Records all routes present in the directory 'routes'
+const routesDirectory = path.join(__dirname, 'routes');
+const routeFiles = fs.readdirSync(routesDirectory);
 
-index.get("/", async (req,res) => {
-  res.send("API para cadastro, atualização, listage e remoção de usuário ");
+routeFiles.forEach((file) => {
+  const routePath = path.join(routesDirectory, file);
+  const route = require(routePath);
+  const routeName = file.split('.')[0]; // Assumede that the file name is the name of the route (without extension)
+
+  app.use(`/${routeName}`, route);
 });
 
-index.listen(8000, () => {
-  console.log("Server started on port 8000: http://localhots:8000");
+app.get("/", (req, res) => {
+  res.send("API para cadastro, atualização, listagem e remoção de usuário");
+});
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}: http://localhost:${port}`);
 });
