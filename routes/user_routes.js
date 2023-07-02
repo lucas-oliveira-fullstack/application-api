93748bcfa3d4a24b59087216b1cee20ef506bff1
Lucas Oliveira <loliveira@iMac-de-Lucas.bbrouter>
2023-07-02 02:32:16 -0300

    Optimizing user_routers.js

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -3,80 +3,116 @@ const User = require('../models/user.js');
 
 const router = express.Router();
 
-//User Resgister
+// User Register
 router.post('/models/user', async (req, res) => {
-    try{
-        const { id, CPF, name, cell_phone, birth_date, age, photo, postal_code, street_name, house_number, complement, neighborhood, city, state, password_18 } = req.body;
-        const user = await User.create({ id, CPF, name, cell_phone, birth_date, age, photo, postal_code, street_name, house_number, complement, neighborhood, city, state, password_18 });
-        res.json(user);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: 'Erro ao criar usuário'});
-    }
+  try {
+    const {
+      id,
+      CPF,
+      name,
+      cell_phone,
+      birth_date,
+      age,
+      photo,
+      postal_code,
+      street_name,
+      house_number,
+      complement,
+      neighborhood,
+      city,
+      state,
+      password_18
+    } = req.body;
+
+    const user = await User.create({
+      id,
+      CPF,
+      name,
+      cell_phone,
+      birth_date,
+      age,
+      photo,
+      postal_code,
+      street_name,
+      house_number,
+      complement,
+      neighborhood,
+      city,
+      state,
+      password_18
+    });
+
+    res.json(user);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Erro ao criar usuário' });
+  }
 });
 
-//Show all Users
+// Show all Users
 router.get('/models/user', async (req, res) => {
-    try {
-        const user = await User.findAll();
-        res.json(user);
-    } catch {
-        console.error(err);
-        res.status(500).json({ error: 'Error ao listar todos os usuários' });
-    }
+  try {
+    const users = await User.findAll();
+    res.json(users);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Erro ao listar todos os usuários' });
+  }
 });
 
-//Show User by ID
+// Show User by ID
 router.get('/models/user/:id', async (req, res) => {
-    try {
-        const userId = req.params.id;
-        const user = await User.findByPk(userId);
-        if (user) {
-            res.json(user);
-        } else {
-            res.status(404).json({ error: 'Usuário não encontrado' });
-        }
-    } catch {
-        console.error(err);
-        res.status(500).json({ error: 'Erro ao encontrar Usuário' });
+  try {
+    const userId = req.params.id;
+    const user = await User.findByPk(userId);
+    if (user) {
+      res.json(user);
+    } else {
+      res.status(404).json({ error: 'Usuário não encontrado' });
     }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Erro ao encontrar Usuário' });
+  }
 });
 
 // Update User by ID
 router.put('/models/user/:id', async (req, res) => {
-    try {
-        const userId = req.params.id;
-        const { name, birth_date, cell_phone, password_18 } = req.body;
-        const [updateRowsCount] = await User.update(
-            {name, birth_date, cell_phone, password_18},
-            { where: {id: userId}}
-        );
-        if (updateRowsCount === 1) {
-            res.json({ message: 'Usuário atualizado com sucesso' });
-        }else {
-            res.status(404).json({ error: 'Usuário não encontrado' });
-        }
-    }catch {
-        console.error(err);
-        res.status(500).json ({ error: 'Erro ao atualizar usuário' });
+  try {
+    const userId = req.params.id;
+    const { name, birth_date, cell_phone, password_18 } = req.body;
+
+    const [updateRowsCount] = await User.update(
+      { name, birth_date, cell_phone, password_18 },
+      { where: { id: userId } }
+    );
+
+    if (updateRowsCount === 1) {
+      res.json({ message: 'Usuário atualizado com sucesso' });
+    } else {
+      res.status(404).json({ error: 'Usuário não encontrado' });
     }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Erro ao atualizar usuário' });
+  }
 });
 
-//Delete User by ID
+// Delete User by ID
 router.delete('/models/user/:id', async (req, res) => {
-    try {
-        const userId = req.params.id;
-        const deletedRowCount = await User.destroy({ where: { id: userId } });
-        if (deletedRowCount === 1) {
-            res.json({ message: 'Usuário excluído com sucesso' });
-        } else {
-            res.status(404).json({ error: 'Usuário não encontrado' });
-        }
-    } catch {
-        console.error(err);
-        res.status(500).json({ error: 'Erro ao excluir o usuário' });
+  try {
+    const userId = req.params.id;
+    const deletedRowCount = await User.destroy({ where: { id: userId } });
+
+    if (deletedRowCount === 1) {
+      res.json({ message: 'Usuário excluído com sucesso' });
+    } else {
+      res.status(404).json({ error: 'Usuário não encontrado' });
     }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Erro ao excluir o usuário' });
+  }
 });
 
-module.exports = router
-
+module.exports = router;
